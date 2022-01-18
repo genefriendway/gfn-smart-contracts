6d7b1d195162df319d48b800c747c90606198662
hungnv132 <hungnv132@gmail.com>
2022-01-18 11:57:30 +0700

    update scripts to deploy smart contracts 3 environment: local, nighlty, production

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -11,53 +11,71 @@ from brownie import (
 )
 from constants import ContractName
 
-# load environment variables from .env.
-load_dotenv()
+gfn_deployer = None
+gfn_deployer_info = None
+ENV_NAME = None
+GFN_DEPLOYER_PRIVATE_KEY = None
+GFN_OWNER_ADDRESS = None
+GFN_FOUNDER_ADDRESS_ONE = None
+GFN_FOUNDER_ADDRESS_TWO = None
+GFN_FOUNDER_ADDRESS_THREE = None
 
-GFN_DEPLOYER_PRIVATE_KEY = os.getenv('GFN_DEPLOYER_PRIVATE_KEY')
-GFN_OWNER_ADDRESS = os.getenv('GFN_OWNER_ADDRESS')
-GFN_FOUNDER_ADDRESS_ONE = os.getenv('GFN_FOUNDER_ADDRESS_ONE')
-GFN_FOUNDER_ADDRESS_TWO = os.getenv('GFN_FOUNDER_ADDRESS_TWO')
-GFN_FOUNDER_ADDRESS_THREE = os.getenv('GFN_FOUNDER_ADDRESS_THREE')
 
-# ========== always validate environment before deploy =======
-errors = []
-if not GFN_DEPLOYER_PRIVATE_KEY:
-    errors.append("Please setup env: 'GFN_DEPLOYER_PRIVATE_KEY'")
-if not GFN_OWNER_ADDRESS:
-    errors.append("Please setup env: 'GFN_OWNER_ADDRESS'")
-if not GFN_FOUNDER_ADDRESS_ONE:
-    errors.append("Please setup env: 'GFN_FOUNDER_ADDRESS_ONE'")
-if not GFN_FOUNDER_ADDRESS_TWO:
-    errors.append("Please setup env: 'GFN_FOUNDER_ADDRESS_TWO'")
-if not GFN_FOUNDER_ADDRESS_THREE:
-    errors.append("Please setup env: 'GFN_FOUNDER_ADDRESS_THREE'")
+def validate_environment_variables():
+    global gfn_deployer
+    global gfn_deployer_info
+    global GFN_DEPLOYER_PRIVATE_KEY
+    global ENV_NAME
+    global GFN_OWNER_ADDRESS
+    global GFN_FOUNDER_ADDRESS_ONE
+    global GFN_FOUNDER_ADDRESS_TWO
+    global GFN_FOUNDER_ADDRESS_THREE
 
-if errors:
-    raise EnvironmentError('\n'.join(errors))
+    ENV_NAME = os.getenv('ENV_NAME')
+    GFN_DEPLOYER_PRIVATE_KEY = os.getenv('GFN_DEPLOYER_PRIVATE_KEY')
+    GFN_OWNER_ADDRESS = os.getenv('GFN_OWNER_ADDRESS')
+    GFN_FOUNDER_ADDRESS_ONE = os.getenv('GFN_FOUNDER_ADDRESS_ONE')
+    GFN_FOUNDER_ADDRESS_TWO = os.getenv('GFN_FOUNDER_ADDRESS_TWO')
+    GFN_FOUNDER_ADDRESS_THREE = os.getenv('GFN_FOUNDER_ADDRESS_THREE')
 
-# Load accounts from private keys
-gfn_deployer = accounts.add(GFN_DEPLOYER_PRIVATE_KEY)
-gfn_deployer_info = {'from': gfn_deployer}
+    errors = []
+    if not GFN_DEPLOYER_PRIVATE_KEY:
+        errors.append("Please setup env: 'GFN_DEPLOYER_PRIVATE_KEY'")
+    if not GFN_OWNER_ADDRESS:
+        errors.append("Please setup env: 'GFN_OWNER_ADDRESS'")
+    if not GFN_FOUNDER_ADDRESS_ONE:
+        errors.append("Please setup env: 'GFN_FOUNDER_ADDRESS_ONE'")
+    if not GFN_FOUNDER_ADDRESS_TWO:
+        errors.append("Please setup env: 'GFN_FOUNDER_ADDRESS_TWO'")
+    if not GFN_FOUNDER_ADDRESS_THREE:
+        errors.append("Please setup env: 'GFN_FOUNDER_ADDRESS_THREE'")
 
-print("===================== DEPLOYMENT INFORMATION =========================")
-print(f'=> Network: {network.show_active()}')
-print(f'=> gfn_deployer: {gfn_deployer}')
-print(f'=> gfn_owner: {GFN_OWNER_ADDRESS}')
-print(f'=> GFN_FOUNDER_ADDRESS_ONE: {GFN_FOUNDER_ADDRESS_ONE}')
-print(f'=> GFN_FOUNDER_ADDRESS_TWO: {GFN_FOUNDER_ADDRESS_TWO}')
-print(f'=> GFN_FOUNDER_ADDRESS_THREE: {GFN_FOUNDER_ADDRESS_THREE}')
-print("======================================================================")
-while True:
-    confirmation = input("[?] Please confirm above information? [yes|no] ")
-    if confirmation.lower().strip() == 'yes':
-        print("=> You selected 'yes' to continue deployment.")
-        break
-    elif confirmation.lower().strip() == 'no':
-        print("====> You selected 'no' to stop deployment.")
-        exit(0)
-    else:
-        pass
+    if errors:
+        raise EnvironmentError('\n'.join(errors))
+
+    # Load accounts from private keys
+
+    gfn_deployer = accounts.add(GFN_DEPLOYER_PRIVATE_KEY)
+    gfn_deployer_info = {'from': gfn_deployer}
+
+    print(F"===================== {ENV_NAME} =========================")
+    print(f'=> Network: {network.show_active()}')
+    print(f'=> gfn_deployer: {gfn_deployer}')
+    print(f'=> gfn_owner: {GFN_OWNER_ADDRESS}')
+    print(f'=> GFN_FOUNDER_ADDRESS_ONE: {GFN_FOUNDER_ADDRESS_ONE}')
+    print(f'=> GFN_FOUNDER_ADDRESS_TWO: {GFN_FOUNDER_ADDRESS_TWO}')
+    print(f'=> GFN_FOUNDER_ADDRESS_THREE: {GFN_FOUNDER_ADDRESS_THREE}')
+    print("===============================================================")
+    while True:
+        confirmation = input("[?] Please confirm above information? [yes|no] ")
+        if confirmation.lower().strip() == 'yes':
+            print("=> You selected 'yes' to continue deployment.")
+            break
+        elif confirmation.lower().strip() == 'no':
+            print("====> You selected 'no' to stop deployment.")
+            exit(0)
+        else:
+            pass
 
 
 # ======= function deploy smart contracts =======
@@ -113,11 +131,14 @@ def deploy_life_treasury(registry):
     return life_treasury
 
 
-def main():
+def _deployment_flow():
+    validate_environment_variables()
     registry = deploy_contract_registry()
     gnft_token = deploy_gnft_token(registry)
     life_token = deploy_life_token(registry)
     life_treasury = deploy_life_treasury(registry)
+
+    # TODO: transfer owner of registry
 
     print("========= RESULTS ==============")
     print(f'=> Network: {network.show_active()}')
@@ -128,3 +149,18 @@ def main():
     print(f"=> LIFEToken Address: {life_token.address}")
     print(f"=> LIFETreasury Address: {life_treasury.address}")
     print("================================")
+
+
+def local():
+    load_dotenv('.env.local')
+    _deployment_flow()
+
+
+def nightly():
+    load_dotenv('.env.nightly')
+    _deployment_flow()
+
+
+def production():
+    load_dotenv('.env.production')
+    _deployment_flow()
