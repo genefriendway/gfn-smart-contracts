09925c63bf5343aeb45b45e555ed2eb8a04f20da
HungGenetica <hung@genefriendway.com>
2022-02-10 13:59:45 +0700

    update unittests for transferring LIFE from ParticipantWallet to external

--- tests/ParticipantWallet/test_transfer_to_user_address.py
+++ tests/ParticipantWallet/test_transfer_to_user_address.py
@@ -8,107 +8,132 @@ def isolation(fn_isolation):
     pass
 
 
-def test_success__transfer_to_gfn_wallet__amount_less_than_balance_of_sender(
+def test_success__transfer_to_user_address__amount_less_than_balance_of_sender(
         deployment, initial_life_treasury, const
 ):
     # Arranges
     gfn_owner1 = deployment[const.GFN_OWNER1]
     life_token = deployment[const.LIFE_TOKEN]
     gpo_wallet = deployment[const.GENETIC_PROFILE_OWNER_WALLET]
-    gfn_sale_wallet = deployment[const.GFN_SALE_WALLET]
     genetic_owner2 = accounts[4]
+    genetic_owner3 = accounts[5]
 
     # Asserts before transferring to GFN Wallet
     assert life_token.balanceOf(gpo_wallet) == 24e+18
-    assert life_token.balanceOf(gfn_sale_wallet) == 0
+    assert life_token.balanceOf(genetic_owner3) == 0
     assert gpo_wallet.getBalanceOfParticipant(genetic_owner2) == 24e+18
 
     # Action: transfer LIFE to GFN wallet
     gpo_wallet.transferExternally(
-        genetic_owner2, gfn_sale_wallet, 5e+18, {"from": gfn_owner1}
+        genetic_owner2, genetic_owner3, 5e+18, {"from": gfn_owner1}
     )
 
     # Asserts after transferring
     assert life_token.balanceOf(gpo_wallet.address) == 19e+18
-    assert life_token.balanceOf(gfn_sale_wallet.address) == 5e+18
+    assert life_token.balanceOf(genetic_owner3.address) == 5e+18
     assert gpo_wallet.getBalanceOfParticipant(genetic_owner2) == 19e+18
 
 
-def test_success__transfer_to_gfn_wallet__amount_equal_to_balance_of_sender(
+def test_success__transfer_to_user_address__amount_equal_to_balance_of_sender(
         deployment, initial_life_treasury, const
 ):
     # Arranges
     gfn_owner1 = deployment[const.GFN_OWNER1]
     life_token = deployment[const.LIFE_TOKEN]
     gpo_wallet = deployment[const.GENETIC_PROFILE_OWNER_WALLET]
-    gfn_sale_wallet = deployment[const.GFN_SALE_WALLET]
     genetic_owner2 = accounts[4]
+    genetic_owner3 = accounts[5]
 
     # Asserts before transferring to GFN Wallet
     assert life_token.balanceOf(gpo_wallet) == 24e+18
-    assert life_token.balanceOf(gfn_sale_wallet) == 0
+    assert life_token.balanceOf(genetic_owner3) == 0
     assert gpo_wallet.getBalanceOfParticipant(genetic_owner2) == 24e+18
 
     # Action: transfer LIFE to GFN wallet
     gpo_wallet.transferExternally(
-        genetic_owner2, gfn_sale_wallet, 24e+18, {"from": gfn_owner1}
+        genetic_owner2, genetic_owner3, 24e+18, {"from": gfn_owner1}
     )
 
     # Asserts after transferring
     assert life_token.balanceOf(gpo_wallet.address) == 0
-    assert life_token.balanceOf(gfn_sale_wallet.address) == 24e+18
+    assert life_token.balanceOf(genetic_owner3.address) == 24e+18
     assert gpo_wallet.getBalanceOfParticipant(genetic_owner2) == 0
 
 
-def test_failure__transfer_to_gfn_wallet__not_gfn_owner_make_transaction(
+def test_success__transfer_to_user_address__user_withdrawal_to_your_own_wallet(
+        deployment, initial_life_treasury, const
+):
+    # Arranges
+    gfn_owner1 = deployment[const.GFN_OWNER1]
+    life_token = deployment[const.LIFE_TOKEN]
+    gpo_wallet = deployment[const.GENETIC_PROFILE_OWNER_WALLET]
+    genetic_owner2 = accounts[4]
+
+    # Asserts before transferring to GFN Wallet
+    assert life_token.balanceOf(gpo_wallet) == 24e+18
+    assert life_token.balanceOf(genetic_owner2) == 0
+    assert gpo_wallet.getBalanceOfParticipant(genetic_owner2) == 24e+18
+
+    # Action: transfer LIFE to GFN wallet
+    gpo_wallet.transferExternally(
+        genetic_owner2, genetic_owner2, 22e+18, {"from": gfn_owner1}
+    )
+
+    # Asserts after transferring
+    assert life_token.balanceOf(gpo_wallet.address) == 2e+18
+    assert life_token.balanceOf(genetic_owner2.address) == 22e+18
+    assert gpo_wallet.getBalanceOfParticipant(genetic_owner2) == 2e+18
+
+
+def test_failure__transfer_to_user_address__not_gfn_owner_make_transaction(
         deployment, initial_life_treasury, const
 ):
     # Arranges
     life_token = deployment[const.LIFE_TOKEN]
     gpo_wallet = deployment[const.GENETIC_PROFILE_OWNER_WALLET]
-    gfn_sale_wallet = deployment[const.GFN_SALE_WALLET]
     genetic_owner2 = accounts[4]
+    genetic_owner3 = accounts[5]
 
     # Asserts before transferring to GFN Wallet
     assert life_token.balanceOf(gpo_wallet) == 24e+18
-    assert life_token.balanceOf(gfn_sale_wallet) == 0
+    assert life_token.balanceOf(genetic_owner3) == 0
     assert gpo_wallet.getBalanceOfParticipant(genetic_owner2) == 24e+18
 
     # Action: transfer LIFE to another participant wallet
     with brownie.reverts("Ownable: caller is not the owner"):
         gpo_wallet.transferExternally(
-            genetic_owner2, gfn_sale_wallet, 24e+18, {"from": genetic_owner2}
+            genetic_owner2, genetic_owner3, 24e+18, {"from": genetic_owner2}
         )
 
     # Asserts after transferring
     assert life_token.balanceOf(gpo_wallet) == 24e+18
-    assert life_token.balanceOf(gfn_sale_wallet) == 0
+    assert life_token.balanceOf(genetic_owner3) == 0
     assert gpo_wallet.getBalanceOfParticipant(genetic_owner2) == 24e+18
 
 
-def test_failure__transfer_to_gfn_wallet__amount_greater_than_balance_of_sender(
+def test_failure__transfer_to_user_address__amount_greater_than_balance_of_sender(
         deployment, initial_life_treasury, const
 ):
     # Arranges
     gfn_owner1 = deployment[const.GFN_OWNER1]
     life_token = deployment[const.LIFE_TOKEN]
     gpo_wallet = deployment[const.GENETIC_PROFILE_OWNER_WALLET]
-    gfn_sale_wallet = deployment[const.GFN_SALE_WALLET]
     genetic_owner2 = accounts[4]
+    genetic_owner3 = accounts[5]
 
     # Asserts before transferring to GFN Wallet
     assert life_token.balanceOf(gpo_wallet) == 24e+18
-    assert life_token.balanceOf(gfn_sale_wallet) == 0
+    assert life_token.balanceOf(genetic_owner3) == 0
     assert gpo_wallet.getBalanceOfParticipant(genetic_owner2) == 24e+18
 
     # Action: transfer LIFE to another participant wallet
     with brownie.reverts("ParticipantWallet: sender has not enough amount "
                          "to send to GFN wallet"):
         gpo_wallet.transferExternally(
-            genetic_owner2, gfn_sale_wallet, 30e+18, {"from": gfn_owner1}
+            genetic_owner2, genetic_owner3, 30e+18, {"from": gfn_owner1}
         )
 
     # Asserts after transferring
     assert life_token.balanceOf(gpo_wallet) == 24e+18
-    assert life_token.balanceOf(gfn_sale_wallet) == 0
+    assert life_token.balanceOf(genetic_owner3) == 0
     assert gpo_wallet.getBalanceOfParticipant(genetic_owner2) == 24e+18
