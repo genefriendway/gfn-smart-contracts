aa8972850febba9dd988698d1ea05d10389eb8e9
HungGenetica <hung@genefriendway.com>
2022-02-14 13:32:33 +0700

    add retriever wallet for GFN, genetic profile owner, investor, lab provider, report provider, data utilizer

---
+++ contracts/mixins/DataUtilizerWalletRetriever.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+
+import "../interfaces/IContractRegistry.sol";
+
+
+abstract contract DataUtilizerWalletRetriever {
+
+    function _getLIFETreasuryAddress(
+        IContractRegistry registry
+    )
+        internal view returns (address)
+    {
+        return registry.getContractAddress('DataUtilizerWallet');
+    }
+
+}
