4f463344f8ad0e112b108c117db34811f89c9d05
HungGenetica <hung@genefriendway.com>
2022-03-30 08:58:33 +0700

    refactor script to verify deployment of smart contracts

--- scripts/verify.py
+++ scripts/verify.py
@@ -34,91 +34,87 @@ class Verification:
 
     def verify_contract_registry(self):
         assert_equal(
-            title="Verify Owner of Registry",
+            title="Registry: Verify Owner",
             value1=self.registry.owner(),
             value2=self.setting.GFN_REGISTRY_OWNER_ADDRESS
         )
 
     def verify_configuration_contract(self):
         assert_equal(
-            title="Verify Owner of Configuration",
+            title=f"Configuration: Verify Register Contract",
+            value1=self.registry.getContractAddress(ContractName.CONFIGURATION),
+            value2=self.configuration.address
+        )
+        assert_equal(
+            title="Configuration: Verify Owner",
             value1=self.configuration.owner(),
             value2=self.setting.GFN_CONFIGURATION_OWNER_ADDRESS
         )
 
-        assert_equal(
-            title=f"Verify Register Contract {ContractName.CONFIGURATION}",
-            value1=self.registry.getContractAddress(ContractName.CONFIGURATION),
-            value2=self.configuration.address
-        )
-
     def verify_gnft_token_contract(self):
         assert_equal(
-            title=f"Verify Register Contract{ContractName.GNFT_TOKEN}",
+            title=f"GNFTToken: Verify Register Contract",
             value1=self.registry.getContractAddress(ContractName.GNFT_TOKEN),
             value2=self.gnft_token.address
         )
         assert_equal(
-            title="Verify Operator of GFNTToken",
+            title="GFNTToken: Verify Operator",
             value1=self.configuration.getOperator(self.gnft_token.address),
             value2=self.setting.GFN_GNFT_OPERATOR_ADDRESS
         )
         assert_equal(
-            title="Verify Holder of Gene-NFT",
+            title="GFNTToken: Verify Holder of Gene-NFT",
             value1=self.configuration.getNFTHolder(),
             value2=self.setting.GFN_NFT_HOLDER_ADDRESS
         )
 
     def verify_life_token_contract(self):
         assert_equal(
-            title=f"Verify Register Contract {ContractName.LIFE_TOKEN}",
+            title=f"LIFEToken: Verify Register Contract",
             value1=self.registry.getContractAddress(ContractName.LIFE_TOKEN),
             value2=self.life_token.address
         )
 
     def verify_life_treasury_contract(self):
         assert_equal(
-            title=f"Verify Register Contract {ContractName.LIFE_TREASURY}",
+            title=f"LIFETreasury: Verify Register Contract",
             value1=self.registry.getContractAddress(ContractName.LIFE_TREASURY),
             value2=self.setting.LIFE_TREASURY_ADDRESS
         )
 
     def verify_gfn_exchange_wallet_contract(self):
-
         assert_equal(
-            title="Verify Operator of GFN Exchange Wallet",
-            value1=self.configuration.getOperator(self.gfn_exchange_wallet.address),
-            value2=self.setting.GFN_EXCHANGE_WALLET_OPERATOR_ADDRESS
-        )
-        assert_equal(
-            title=f"Verify Register Contract {ContractName.GFN_EXCHANGE_WALLET}",
+            title=f"GFNExchangeWallet: Verify Register Contract",
             value1=self.registry.getContractAddress(ContractName.GFN_EXCHANGE_WALLET),
             value2=self.gfn_exchange_wallet.address
         )
+        assert_equal(
+            title="GFNExchangeWallet: Verify Operator",
+            value1=self.configuration.getOperator(self.gfn_exchange_wallet.address),
+            value2=self.setting.GFN_EXCHANGE_WALLET_OPERATOR_ADDRESS
+        )
 
     def verify_gfn_profit_wallet_contract(self):
-
         assert_equal(
-            title="Verify Operator of GFN Profit Wallet",
+            title=f"GFNProfitWallet: Verify Register Contract",
+            value1=self.registry.getContractAddress(ContractName.GFN_PROFIT_WALLET),
+            value2=self.gfn_profit_wallet.address
+        )
+        assert_equal(
+            title="GFNProfitWallet: Verify Operator",
             value1=self.configuration.getOperator(self.gfn_profit_wallet.address),
             value2=self.setting.GFN_PROFIT_WALLET_OPERATOR_ADDRESS
         )
 
-        assert_equal(
-            title=f"Verify Register Contract {ContractName.GFN_PROFIT_WALLET}",
-            value1=self.registry.getContractAddress(ContractName.GFN_PROFIT_WALLET),
-            value2=self.gfn_profit_wallet.address
-        )
-
     def verify_gfn_exchange_life_bank_contract(self):
         assert_equal(
-            title="Verify Operator of GFN Exchange LIFE Bank",
+            title=f"GFNExchangeLIFEBank: Verify Register Contract",
+            value1=self.registry.getContractAddress(
+                ContractName.GFN_EXCHANGE_LIFE_BANK),
+            value2=self.gfn_exchange_life_bank.address
+        )
+        assert_equal(
+            title="GFNExchangeLIFEBank: Verify Operator",
             value1=self.configuration.getOperator(self.gfn_exchange_life_bank.address),
             value2=self.setting.GFN_EXCHANGE_LIFE_BANK_OPERATOR_ADDRESS
-        )
-
-        assert_equal(
-            title=f"Verify Register Contract {ContractName.GFN_EXCHANGE_LIFE_BANK}",
-            value1=self.registry.getContractAddress(ContractName.GFN_EXCHANGE_LIFE_BANK),
-            value2=self.gfn_exchange_life_bank.address
         )
