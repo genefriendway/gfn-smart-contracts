5d876e5393c6f60e7b9e28e63d3430985fba29e7
HungGenetica <hung@genefriendway.com>
2022-09-23 13:24:11 +0700

    add more unittests for PCSPReward Contract

--- tests/PCSP/conftest.py
+++ tests/PCSP/conftest.py
@@ -2,7 +2,13 @@
 
 import pytest
 
-from brownie import accounts, PCSPConfiguration, PCSPReward
+from brownie import (
+    accounts,
+    PCSPConfiguration,
+    PCSPReward,
+    GenomicDAOToken,
+    TokenWallet
+)
 
 
 @pytest.fixture(scope="module")
@@ -10,20 +16,47 @@ def pcsp_deployment():
     deployer = accounts[0]
     pcsp_reward_owner = accounts[1]
     pcsp_configuration_owner = accounts[2]
+    dao_token_owner = accounts[3]
+    dao_token_cap = 1000000000 * 10 ** 18  # one billion
+    token_wallet_owner = accounts.add()
 
-    # deploy smart contracts and get instance of them
+    # Deploy PCSPConfiguration contract
     pcsp_configuration_contract = PCSPConfiguration.deploy(
         pcsp_configuration_owner,
         {"from": deployer}
     )
 
+    # Deploy PCSPReward contract
     pcsp_reward_contract = PCSPReward.deploy(
         pcsp_reward_owner,
         pcsp_configuration_contract.address,
         {"from": deployer}
     )
 
+    # Deploy GenomicDAOToken contract
+    dao_token_contract = GenomicDAOToken.deploy(
+        dao_token_owner,
+        "Token Name",
+        "TOKEN",
+        dao_token_cap,
+        {"from": deployer}
+    )
+    dao_token_contract.mint(
+        dao_token_owner, 100000000 * 10 ** 18, {"from": dao_token_owner}
+    )
+
+    # Deploy TokenWallet contract
+    token_wallet_contract = TokenWallet.deploy(
+        token_wallet_owner,
+        dao_token_contract,
+        {"from": deployer}
+    )
+
     results = {
+        'dao_token_owner': dao_token_owner,
+        'dao_token_contract': dao_token_contract,
+        'token_wallet_owner': token_wallet_owner,
+        'token_wallet_contract': token_wallet_contract,
         'pcsp_reward_owner': pcsp_reward_owner,
         'pcsp_reward_contract': pcsp_reward_contract,
         'pcsp_configuration_owner': pcsp_configuration_owner,
